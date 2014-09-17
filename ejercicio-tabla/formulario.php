c356ac8b1c3b3b793c98ed81fcf855a3643bf6fa
zmwebdev <zmwebdev@gmail.com>
2014-09-17 10:57:53 +0200

    formulario

---
+++ ejercicio-tabla/formulario.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+
+    <?php $num = $_GET["num"]; ?>
+	<br>
+
+	<table border="1">
+		<?php
+		for ($x=0;$x<=$num;$x++) {
+			echo "<tr>
+		    <td>Jill</td>
+		    <td>Smith</td>
+		  	</tr>";
+		}
+		?>
+	</table>
+
+</body>
+</html>
