8f9bc243cfd390952c603f83d0f8b4937ade97b6
zmwebdev <zmwebdev@gmail.com>
2014-09-17 12:01:57 +0200

    tablas con error

--- ejercicio-tabla/formulario.php
+++ ejercicio-tabla/formulario.php
@@ -1,19 +1,30 @@
 <html>
 <body>
 
-    <?php $num = $_GET["num"]; ?>
-	<br>
+<?php
+// No funciona cuando se envia un valor no numerico!!!
+$num = 0;
+if(isset($_GET["num"])){
+    $num = $_GET["num"];
+} else {
+    $num = 1;
+}
 
-	<table border="1">
-		<?php
-		for ($x=0;$x<=$num;$x++) {
-			echo "<tr>
-		    <td>Jill</td>
-		    <td>Smith</td>
-		  	</tr>";
-		}
-		?>
-	</table>
+?>
+
+<br>
+
+<table border="1">
+	<?php
+	for ($x=0;$x<$num;$x++) {
+		echo "<tr>
+
+	    <td>Jill</td>
+	    <td>Smith</td>
+	  	</tr>";
+	}
+	?>
+</table>
 
 </body>
 </html>
