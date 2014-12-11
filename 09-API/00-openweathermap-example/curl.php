f86e6964d248d88453073cff1b8eda13dce8277b
zmwebdev <zmwebdev@gmail.com>
2014-12-11 21:42:09 +0100

    curl

--- 09-API/00-openweathermap-example/curl.php
+++ 09-API/00-openweathermap-example/curl.php
@@ -1,13 +1,18 @@
 <?php
-// http://www.lornajane.net/posts/2010/three-ways-to-make-a-post-request-from-php
-$url = 'http://api.openweathermap.org/data/2.5/weather?lat=43.3276658&lon=-1.9711435';
+// http://codular.com/curl-with-php
+// Get cURL resource
+$curl = curl_init();
+// Set some options - we are passing in a useragent too here
+curl_setopt_array($curl, array(
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => 'http://api.openweathermap.org/data/2.5/weather?lat=43.3276658&lon=-1.9711435',
+    //CURLOPT_USERAGENT => 'Codular Sample cURL Request'
+));
+// Send the request & save response to $resp
+$resp = curl_exec($curl);
+// Close request to clear up some resources
+curl_close($curl);
 
-$ch = curl_init($url);
- 
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
- 
-$response = curl_exec($ch);
-curl_close($ch);
+
+print_r($resp);
 ?>
