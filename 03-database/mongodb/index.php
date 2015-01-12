df7ced63e3edb5d9af139f6b9ae862ced0d70f64
zmwebdev <zmwebdev@gmail.com>
2015-01-12 13:49:24 +0100

    php mongodb

--- 03-database/mongodb/index.php
+++ 03-database/mongodb/index.php
@@ -3,4 +3,30 @@
 
 $m = new MongoClient("mongodb://test:test@ds033400.mongolab.com:33400/zmwebdev-test");
 
+$db = $m->selectDB('zmwebdev-test');
+
+$collection = $db->books;
+// add a record
+$document = array( "title" => "Calvin and Hobbes", "author" => "Bill Watterson" );
+$collection->insert($document);
+
+// add another record, with a different "shape"
+$document = array( "title" => "XKCD", "online" => true );
+$collection->insert($document);
+
+// find everything in the collection
+$cursor = $collection->find();
+
+foreach ( $cursor as $id => $value )
+{
+    echo "$id: ";
+    var_dump( $value );
+}
+
+// iterate through the results
+/*
+foreach ($cursor as $document) {
+    echo $document["title"] . "\n";
+}
+*/
 ?>
