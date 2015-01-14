fde1aa38e4dd35068b7badabe82f894bf07660fb
zmwebdev <zmwebdev@gmail.com>
2015-01-14 12:02:06 +0100

    slim RESTful

---
+++ 11-RESTful-API/slim/README.md
@@ -0,0 +1,13 @@
+http://docs.slimframework.com/
+
+Install:
+
+```
+$ composer install
+```
+
+Run:
+
+```
+$ php5 index.php
+``` 
---
+++ 11-RESTful-API/slim/index.php
@@ -0,0 +1,15 @@
+<?php
+require 'vendor/autoload.php';
+
+//require 'Slim/Slim.php';
+//\Slim\Slim::registerAutoloader();
+
+$app = new \Slim\Slim();
+
+$app->get('/hello/:name', function ($name) {
+    echo "Hello, $name";
+});
+
+$app->run();
+
+?>
