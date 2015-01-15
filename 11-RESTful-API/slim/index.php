9ac4d90bba0b6c0e3799a73ccd168f9ebace52bc
zmwebdev <zmwebdev@gmail.com>
2015-01-15 12:41:32 +0100

    json encode

--- 11-RESTful-API/slim/index.php
+++ 11-RESTful-API/slim/index.php
@@ -10,6 +10,15 @@ $app->get('/hello/:name', function ($name) {
     echo "Hello, $name";
 });
 
+$app->get('/hello2/:name', function ($name) {
+    // echo "Hello, $name";
+
+    header('Content-Type: application/json');
+    $data = array('name' => $name, 'surname' => 'Abizen');
+	echo json_encode($data);
+});
+
+
 $app->run();
 
 ?>
