04b43ea8c0a075309cf4d8018e7e03b8892e32d8
zmwebdev <zmwebdev@gmail.com>
2015-09-11 11:19:55 +0200

    new file

---
+++ 00-HelloWorld/strings.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>HelloWorld</title>
+</head>
+<body>
+
+<?php
+
+$izena = "Koxme";
+$abizena = "Mendia";
+
+$izenAbizena = $izena . $abizena;
+
+echo "El nombre es: $izena y tiene 30 años";
+echo "<br>";
+echo 'El nombre es: $izena y tiene 30 años';
+echo "<br>";
+echo "$izena" . " " . "$abizena"
+echo "<br>";
+echo "$izenAbizena";
+?>
+
+</body>
+</html>
