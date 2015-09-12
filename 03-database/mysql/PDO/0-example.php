67bc5ea8bfe490cd5c824dd53d2364e40244b48a
zmwebdev <zmwebdev@gmail.com>
2015-09-12 12:18:43 +0200

    Update 0-example.php

--- 03-database/mysql/PDO/0-example.php
+++ 03-database/mysql/PDO/0-example.php
@@ -1,5 +1,7 @@
 <?php
 // PDO + MySQL
+// http://www.phptherightway.com/#databases
+
 $pdo = new PDO('mysql:host=example.com;dbname=database', 'user', 'password');
 $statement = $pdo->query("SELECT some_field FROM some_table");
 $row = $statement->fetch(PDO::FETCH_ASSOC);
