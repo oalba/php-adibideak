54903941209f50f96128fc5a3879e05f71d12750
zmwebdev <zmwebdev@gmail.com>
2015-09-18 11:25:13 +0200

    Update db.sql

--- 03-database/db.sql
+++ 03-database/db.sql
@@ -6,4 +6,5 @@ surname varchar(255),
 email varchar(255) NOT NULL
 );
 
-INSERT INTO users (userid,name,surname,email) VALUES (0,'test','test','test@test.com');
+INSERT INTO users (userid,name,surname,email) VALUES (0,'Peru','test','peru@test.com');
+INSERT INTO users (userid,name,surname,email) VALUES (0,'Koxme','test','koxme@test.com');
