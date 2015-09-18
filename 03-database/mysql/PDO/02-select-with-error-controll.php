6a33dbc24fc4a60b615d0bc2d9f355325fad54c0
zmwebdev <zmwebdev@gmail.com>
2015-09-18 20:01:06 +0200

    Create 02-select-with-error-controll.php

---
+++ 03-database/mysql/PDO/02-select-with-error-controll.php
@@ -0,0 +1,33 @@
+<?php
+//To Test
+try {
+
+  $servername = '';
+  $username = '';
+  $password = '';
+  $database = '';
+  $dbport = '';
+  
+  // Create connection
+  $db = new PDO("mysql:host=$servername;dbname=$database;port=$dbport;charset=utf8",
+			$username,
+			$password);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	$stmt = $db->prepare('SELECT * FROM users WHERE userid = :userid');
+	$userid = filter_input(INPUT_GET, 'userid', FILTER_VALIDATE_INT); 
+	
+	if ($userid === false) {
+		echo "Error: not INPUT_GET userid";
+	} else {
+		$stmt->bindParam(':userid', $userid, PDO::PARAM_INT); // <-- Automatically sanitized for SQL by PDO
+		$stmt->execute();
+		$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		echo json_encode($result);
+	}
+	
+} catch (PDOException $e) {
+	echo $e->getMessage();
+}
+?>
