f99657950c39f0161fc1905817d3731d1009fd77
zmwebdev <zmwebdev@gmail.com>
2015-10-05 11:16:57 +0200

    mvc

--- 13-MVC/example/example.php
+++ 13-MVC/example/example.php
@@ -1,4 +1,8 @@
 <?php
+include_once("model.php");
+include_once("controller.php");
+include_once("view.php");
+
 $model = new Model();
 $controller = new Controller($model);
 $view = new View($controller, $model);
