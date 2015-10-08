4bfa9c22c1bad328c39591dd3fdb4021fceab3de
zmwebdev <zmwebdev@gmail.com>
2015-10-08 09:07:56 +0200

    index

--- 11-RESTful-API/slim/index.php
+++ 11-RESTful-API/slim/index.php
@@ -5,6 +5,12 @@ require 'vendor/autoload.php';
 //\Slim\Slim::registerAutoloader();
 
 $app = new \Slim\Slim();
+$app->config('debug', false);
+
+
+$app->get('/', function () {
+    echo "Use: http://server/index.php/";
+});
 
 $app->get('/hello/:name', function ($name) {
     echo "Hello, $name";
@@ -19,6 +25,17 @@ $app->get('/hello2/:name', function ($name) {
 	echo json_encode($data);
 });
 
+$app->get('/archive(/:year(/:month(/:day)))', function ($year = 2010, $month = 12, $day = 05) {
+    echo sprintf('%s-%s-%s', $year, $month, $day);
+});
+
+//POST variable
+// http://php.net/manual/en/functions.anonymous.php
+$app->post('/user/add', function () use ($app) {
+	$username = $app->request->post('username'); 
+    echo "HTTP post, username: $username";
+
+});
 
 $app->run();
 
