1a66ffbd5c7575741d04170d67f50ffe1bcf9f5f
zmwebdev <zmwebdev@gmail.com>
2015-11-16 12:05:49 +0000

    slim twig

--- 11-RESTful-API/slim/03-slim-twig/index.php
+++ 11-RESTful-API/slim/03-slim-twig/index.php
@@ -17,6 +17,14 @@ $app->get('/', function () use ($twig) {
     // echo "hello";
 });
 
+$app->get('/test/test2/', function () use ($twig) {
+    echo $twig->render('index.html', array('url' => '/test/test2/'));
+
+    // var_dump($twig);
+    // echo "hello";
+});
+
+
 $app->run();
 
 ?>
